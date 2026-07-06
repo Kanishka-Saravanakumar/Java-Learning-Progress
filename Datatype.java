2ac35ccddb76ca91e8618760e3a144017e39d2f8
Kanishkka <kanishka.saravanakumar@gmail.com>
2026-07-06 17:13:02 +0000

    Add my Java Code

---
+++ Datatype.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+public class Datatype {
+    public static void main(String 
+        []args){
+        int a=10;
+        Date today=new Date();
+        System.out.println(today);
+    }
+}
